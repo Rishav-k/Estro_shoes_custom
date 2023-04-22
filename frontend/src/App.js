de81ef80d419f0a1ad13865c57e06e186592fb47
Rishav Kumar <rishav_k@ce.iitr.ac.in>
2023-04-22 12:38:49 +0530

    basic ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 import Home from './components/Home.js';
-import Navbar from './componenets/Navbar.js';
+import NavBar from './components/Navbar.js';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App"> 
-    <Navbar/>
+    <NavBar/>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}>
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,9 +1,29 @@
 import React from 'react'
-
+import {AiOutlineArrowLeft }from  "react-icons/ai"
+import {AiOutlineArrowRight }from  "react-icons/ai"
+import "../css/home.css"
 export default function Home() {
   return (
     <div>
-      
+      <div className="home-top">
+        home top
+      </div>
+      <div><center>
+        <div>
+            <AiOutlineArrowLeft/>
+            <span> Vamp 1/4</span>
+           <AiOutlineArrowRight/>
+           
+        </div></center>
+        <div> 
+        <center><button>Sceude</button>
+        <button>Meusium Calf</button></center>
+        </div>
+        <div>
+            <button className=''>black</button>
+            <button className=''>blue</button>
+        </div>
+      </div>
     </div>
   )
 }
